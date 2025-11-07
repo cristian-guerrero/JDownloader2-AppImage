2a2992003c46e0133b59b2b561ed7735b1d8088b
Shikakiben <shikakiben@example.com>
2025-11-07 22:48:32 +0100

    fix: crée le lien gunzip -> gzip pour Arch minimal

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 set -e
-pacman -Sy --noconfirm wget p7zip gzip
+pacman -Sy --noconfirm wget p7zip gzip gunzip
+
+# Correction gunzip manquant sur certains Arch minimal
+ln -sf /usr/bin/gzip /usr/bin/gunzip
 
