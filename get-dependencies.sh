7aad433fa4bc4a804c840b6b449b35ee1c579e73
Shikakiben <108907095+Shikakiben@users.noreply.github.com>
2025-11-14 20:59:34 +0100

    Replace curl with wget for JRE URL retrieval

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -6,7 +6,7 @@ ARCH="$(uname -m)"
 DEBLOATED_PKGS_INSTALLER="https://raw.githubusercontent.com/pkgforge-dev/Anylinux-AppImages/refs/heads/main/useful-tools/get-debloated-pkgs.sh"
 JDOWNLOADER_JAR="https://installer.jdownloader.org/JDownloader.jar"
 JRE_API_URL="https://api.adoptium.net/v3/assets/latest/25/hotspot?architecture=x64&heap_size=normal&image_type=jre&jvm_impl=hotspot&os=linux&vendor=adoptium"
-JRE_URL=$(curl -sSL --retry-connrefused --tries=30 "$JRE_API_URL" | jq -r '.[0].binary.package.link')
+JRE_URL=$(wget -qO- --retry-connrefused --tries=30 "$JRE_API_URL" | jq -r '.[0].binary.package.link')
 
 echo "Installing build dependencies for sharun & AppImage integration..."
 echo "---------------------------------------------------------------"
