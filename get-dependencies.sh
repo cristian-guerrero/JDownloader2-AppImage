641bc2830d40063f7b1173c1505aba0fa0ceb28e
Shikakiben <108907095+Shikakiben@users.noreply.github.com>
2025-11-14 20:33:06 +0100

    Refactor get-dependencies.sh for improved installation
    Updated dependency installation and added error handling for downloads.

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -2,17 +2,39 @@
 
 set -eux
 
+ARCH="$(uname -m)"
+DEBLOATED_PKGS_INSTALLER="https://raw.githubusercontent.com/pkgforge-dev/Anylinux-AppImages/refs/heads/main/useful-tools/get-debloated-pkgs.sh"
+JDOWNLOADER_JAR="https://installer.jdownloader.org/JDownloader.jar"
+JRE_API_URL= "https://api.adoptium.net/v3/assets/latest/25/hotspot?architecture=x64&heap_size=normal&image_type=jre&jvm_impl=hotspot&os=linux&vendor=adoptium"
+JRE_URL=$(curl -sSL --retry-connrefused --tries=30 "$JRE_API_URL" | jq -r '.[0].binary.package.link')
 
-
-# Installer les dépendances nécessaires pour le build AppImage et quick-sharun
+echo "Installing build dependencies for sharun & AppImage integration..."
+echo "---------------------------------------------------------------"
 pacman -Syu --noconfirm \
-    wget \
+	base-devel \
+	curl \
+	desktop-file-utils \
+	git \
+	libxtst \
+	wget \
+	xorg-server-xvfb \
+	zsync
     p7zip \
     gzip \
     which \
-    desktop-file-utils \
-    libxtst \
-    zsync \
     jq
+   
 
-echo "Dépendances et JDownloader2 installés."
+echo "Installing the app & it's dependencies..."
+echo "---------------------------------------------------------------"
+
+wget --retry-connrefused --tries=30 "$JDOWNLOADER_JAR -0 ./JDownloader.jar
+
+wget --retry-connrefused --tries=30 "$JRE_URL" -O ./jre.tar.gz 
+
+
+echo "Installing debloated packages..."
+echo "---------------------------------------------------------------"
+wget --retry-connrefused --tries=30 "$DEBLOATED_PKGS_INSTALLER" -O ./get-debloated-pkgs.sh
+chmod +x ./get-debloated-pkgs.sh
+./get-debloated-pkgs.sh libxml2-mini mesa-nano gtk4-mini gdk-pixbuf2-mini librsvg-mini opus-mini
