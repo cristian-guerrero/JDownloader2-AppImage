28aefb5d836020c583e3d94a005510b58b92fa9a
Shikakiben <108907095+Shikakiben@users.noreply.github.com>
2025-11-16 11:38:41 +0100

    Remove unnecessary './' from quick-sharun commands

--- make-appimage.sh
+++ make-appimage.sh
@@ -11,7 +11,7 @@ export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}
 export JD2_APPIMAGE_BUILD=1
 
 # Deploy dependencies
-./quick-sharun /AppDir/bin/JDownloader2 /AppDir/JDownloader.jar /AppDir/jre.tar.gz
+quick-sharun /AppDir/bin/JDownloader2 /AppDir/JDownloader.jar /AppDir/jre.tar.gz
 
 # Make the AppImage with uruntime
-./quick-sharun --make-appimage
+quick-sharun --make-appimage
